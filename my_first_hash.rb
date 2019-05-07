9ca39f7cd41faf7769933fa890791d23d53d69bd
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-07 22:16:57 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,8 +22,8 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest << ["muskets"] = 2 
-  shipping_manifest << ["gun_powder"] = 4
+  shipping_manifest  ["muskets"] = 2 
+  shipping_manifest  ["gun_powder"] = 4
   
   shipping_manifest
 end
