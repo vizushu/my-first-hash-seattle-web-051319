c7ad5d13ab39db9184f50bd09c4601737f6a0426
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-07 22:09:08 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,6 +4,8 @@ end
 
 
 def shipping_manifest
+  the_manifest = siblings
+  
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
